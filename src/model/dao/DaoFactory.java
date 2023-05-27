671583d834cf19478155e819dfec8a8a1581fb80
José <josejr017@gmail.com>
2023-05-27 14:14:11 -0300

    Implementação da classe DaoFactory

---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,20 @@
+package model.dao;
+
+import model.dao.impl.CompradorDaoImpl;
+import model.dao.impl.LojaDaoImpl;
+import model.dao.impl.ProdutoDaoImpl;
+
+public class DaoFactory {
+
+	public static CompradorDao criarCompradorDao() {
+		return new CompradorDaoImpl();
+	}
+	
+	public static LojaDao criarLojaDao() {
+		return new LojaDaoImpl();
+	}
+	
+	public static ProdutoDao criarProdutoDao() {
+		return new ProdutoDaoImpl();
+	}
+}
