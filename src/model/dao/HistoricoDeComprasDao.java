ac1945b68a05ba58ba6b5c8ac776346b845fcb48
Adriana Sousa <120699927+Adriana-Sousa@users.noreply.github.com>
2023-06-10 13:36:33 -0300

    Add files via upload

---
+++ src/model/dao/HistoricoDeComprasDao.java
@@ -0,0 +1,11 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Comprador;
+import model.entities.Compra;
+
+public interface HistoricoDeComprasDao {
+    void adicionar(Comprador comprador, Compra compra);
+    List<Compra> verHistorico(Comprador comprador);
+}
