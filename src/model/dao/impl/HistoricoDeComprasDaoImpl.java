62f1b4f35a70b7eae724b78f11988aac5021ea10
Adriana Sousa <120699927+Adriana-Sousa@users.noreply.github.com>
2023-06-10 13:38:37 -0300

    Add HistoricoDeComprasDaoImpl

---
+++ src/model/dao/impl/HistoricoDeComprasDaoImpl.java
@@ -0,0 +1,36 @@
+package model.dao.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.dao.CompradorDao;
+import model.dao.DaoFactory;
+import model.dao.HistoricoDeComprasDao;
+import model.entities.Comprador;
+import model.entities.Compra;
+
+public class HistoricoDeComprasDaoImpl implements HistoricoDeComprasDao{
+
+    private static CompradorDao compradorDao = DaoFactory.criarCompradorDao();
+
+	@Override
+	public void adicionar(Comprador comprador, Compra compra) {
+		List<Compra> historicos = comprador.getHistoricoDeCompras();
+
+		if (historicos == null) {
+			historicos = new ArrayList<>();
+		} 
+		historicos.add(compra);
+
+		comprador.setHistoricoDeCompras(historicos);
+
+		compradorDao.atualizar(comprador);
+		
+	}
+
+	@Override
+	public List<Compra> verHistorico(Comprador comprador) {
+		return comprador.getHistoricoDeCompras();
+	}
+  
+}
