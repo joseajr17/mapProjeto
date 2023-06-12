ccc1a4ed74dcc335221bfa3dac57ff17678fced6
Adriana Sousa <120699927+Adriana-Sousa@users.noreply.github.com>
2023-06-12 15:58:38 -0300

    Update CarrinhoDeComprasDao.java

--- src/model/dao/CarrinhoDeComprasDao.java
+++ src/model/dao/CarrinhoDeComprasDao.java
@@ -2,6 +2,7 @@ package model.dao;
 
 import java.util.List;
 
+import exception.StoreNotFoundException;
 import model.entities.Comprador;
 import model.entities.Produto;
 
@@ -10,6 +11,6 @@ public interface CarrinhoDeComprasDao {
 	void adicionar(Comprador comprador, Produto obj);
 	void remover(Comprador comprador, Produto obj);
 	List<Produto> listarProdutos(Comprador comprador);
-	void comprar(Comprador comprador, Produto obj);
+	void comprar(Comprador comprador, Produto obj, int quantidade) throws StoreNotFoundException;
 
 }
